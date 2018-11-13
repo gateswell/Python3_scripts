74d324569255f6a8e98ddd656479fef127e6d92b
Shuhuan Cao <caoshuhuan@yeah.net>
2018-11-13 13:54:54 +0800

    Update ReverseComplement.py

--- ReverseComplement.py
+++ ReverseComplement.py
@@ -3,10 +3,10 @@ import sys
 
 def Reverse_base(seq):
 	trans ={'A':'T',
-			'T':'A',
-			'G':'C',
-			'C':'G',
-			}
+		'T':'A',
+		'G':'C',
+		'C':'G',
+		}
 	rev_seq=''
 	for i in range((len(seq)-1),-1,-1):
 		rev_seq += trans[seq[i]]
